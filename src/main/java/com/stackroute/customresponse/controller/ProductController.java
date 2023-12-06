03b9537279f7ac7ffa26ffc12247d38db877cd4b
Chulbul.JI <chulbul.ji@cognizant.com>
2023-12-06 10:05:02 +0000

    first commit

--- src/main/java/com/stackroute/customresponse/controller/ProductController.java
+++ src/main/java/com/stackroute/customresponse/controller/ProductController.java
@@ -26,25 +26,49 @@ public class ProductController {
     //use try catch and return ResponseHandler with "Successfully retrieved data!" message
     @GetMapping("/products")
     public ResponseEntity<Object> getAllProduct() {
-      return null;
+//      return null;
+        try {
+            List<Product> products = productService.getAllProduct();
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     //use try catch and return ResponseHandler with "Successfully retrieved data!" message
     @GetMapping("/products/{id}")
     public ResponseEntity < Object > getProductById(@PathVariable long id) {
-       return null;
+//       return null;
+        try {
+            Product product = productService.getProductById(id);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     //use try catch and return ResponseHandler with "Successfully added data!" message
     @PostMapping("/products")
     public ResponseEntity < Object > createProduct(@RequestBody Product product) {
-       return null;
+//       return null;
+        try {
+            Product createdProduct = productService.createProduct(product);
+            return new ResponseEntity<>(createdProduct, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     //use try catch and return ResponseHandler with "Deleted!" message
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable long id) {
-        return null;
+//        return null;
+        try {
+            Product deletedProduct = productService.deleteProduct(id);
+            return new ResponseEntity<>(deletedProduct, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     }
